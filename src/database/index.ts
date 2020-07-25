45257cd08eb91e821afde96986b968db4fad7db5
Ygor Mattos <ygormattos.b@gmail.com>
2020-07-25 11:50:55 -0300

    :recycle: Refactoring

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
-mongoose.connect(
-  'mongodb+srv://linkapi_db:linkapi10@node-linkapi.x29pd.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true },
-);
+const url = process.env.URL_CONNECTION || '';
+mongoose.connect(url, { useNewUrlParser: true });
